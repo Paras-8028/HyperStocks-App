d4d22c548488981191e046a2711d7fa7ac7855c2
Paras Patil <Patilparas8028@gmail.com>
2026-01-17 15:26:19 +0530

    functions added

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {useRouter} from "next/dist/client/components/navigation";
+
+const UserDropdown = () => {
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+
+        router.push("/sign-in");
+    }
+
+    const user = {name: 'John', email: 'contact@jsmastery.com'}
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+export default UserDropdown
