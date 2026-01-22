591af9b75a50241965ba7f8b3796b54d3a51a7d8
Paras Patil <Patilparas8028@gmail.com>
2026-01-22 23:29:54 +0530

    Implemented authenticated logic

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -10,23 +10,22 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {useRouter} from "next/dist/client/components/navigation";
+import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
+import {LogOut} from "lucide-react";
 import NavItems from "@/components/NavItems";
+import {signOut} from "@/lib/actions/auth.actions";
 
-const UserDropdown = () => {
+const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: StockWithWatchlistStatus[]}) => {
     const router = useRouter();
 
     const handleSignOut = async () => {
-
+        await signOut();
         router.push("/sign-in");
     }
 
-    const user = {name: 'John', email: 'contact@jsmastery.com'}
-
     return (
         <DropdownMenu>
-
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
@@ -42,9 +41,7 @@ const UserDropdown = () => {
                     </div>
                 </Button>
             </DropdownMenuTrigger>
-
             <DropdownMenuContent className="text-gray-400">
-
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
@@ -61,18 +58,14 @@ const UserDropdown = () => {
                         </div>
                     </div>
                 </DropdownMenuLabel>
-
                 <DropdownMenuSeparator className="bg-gray-600"/>
-
                 <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
-                    {/*<LogOut className="h-4 w-4 mr-2 hidden sm:block" />*/}
+                    <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
                     Logout
                 </DropdownMenuItem>
-
                 <DropdownMenuSeparator className="hidden sm:block bg-gray-600"/>
-
                 <nav className="sm:hidden">
-                    {/*<NavItems initialStocks={initialStocks} />*/}
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
             </DropdownMenuContent>
         </DropdownMenu>
