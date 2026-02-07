d8511d41b5b75adab42ade4e82c1a370b99cdfeb
Paras Patil <Patilparas8028@gmail.com>
2026-02-07 23:02:12 +0530

    responsive design

--- app/watchlist/components/WatchlistTabs.tsx
+++ app/watchlist/components/WatchlistTabs.tsx
@@ -44,7 +44,6 @@ export default function WatchlistTabs({
             </div>
 
             {/* Tabs Content */}
-
             {active === "Watchlist" && (
                 <WatchlistTable items={items} />
             )}
